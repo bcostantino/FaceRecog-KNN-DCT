892098fc83813d5449092df99d9ecbc07171bf63
bcostantino <costan96@students.rowan.edu>
2021-12-11 20:58:44 -0500

    Better support for surface and config

--- Part4b.m
+++ Part4b.m
@@ -1,27 +1,41 @@
 % Part 4b, using full_eval
 
 % configuration
-plot_type=1;
+plot_type=2;
+filename='eval_data.mat';
+l_start=25;
+l_incriment=15;
+l_num=6;
+k_start=1;
+k_incriment=2;
+k_num=4;
 
 if isfile(filename)
      % File exists.
-     load('eval_data.mat','eval_data');
+     load(filename,'eval_data','lengths','ks');
 else
     % File does not exist.
     eval_data=zeros(24,3);
     counter=1;
-    for i=1:6
-        num_feat=(i-1)*15+25;
-        for j=1:4
-            k=(j-1)*2+1;
+    
+    % define x and y for surface plot
+    lengths=zeros(l_num,1);
+    ks=zeros(k_num,1);
+    
+    for i=1:l_num
+        num_feat=(i-1)*l_incriment+l_start;
+        for j=1:k_num
+            k=(j-1)*k_incriment+k_start;
             [t_labels,p_labels,accuracy]=full_eval(num_feat,k);
 
             eval_data(counter,:)=[num_feat,k,accuracy];
+            ks(j)=k;
             counter=counter+1;
         end
+        lengths(i)=num_feat;
     end
     % save data to file
-    save('eval_data.mat','eval_data');
+    save(filename,'eval_data','lengths','ks');
 end
 
 % plot full eval as function of dimension and k
@@ -32,7 +46,7 @@ if plot_type==1
 % handle surface plot
 elseif plot_type==2
     Z=reshape(eval_data(:,3),4,6);
-    surf([25 40 55 70 85 100],[1 3 5 7],Z);
+    surf(lengths,ks,Z);
 end
 
 xlabel('feature dimension');
